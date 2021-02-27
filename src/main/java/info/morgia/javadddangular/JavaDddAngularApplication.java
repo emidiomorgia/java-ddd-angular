fbcb4f2a98fa8794f62394a6ad92149ea2e3a323
emidio@morgia.info <emidio@morgia.info>
2021-02-27 18:01:06 +0100

    First Commit

---
+++ src/main/java/info/morgia/javadddangular/JavaDddAngularApplication.java
@@ -0,0 +1,13 @@
+package info.morgia.javadddangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDddAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDddAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/morgia/javadddangular/JavaDddAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.javadddangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDddAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
