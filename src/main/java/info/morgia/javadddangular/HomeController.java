1be1eb410bdfb488d03e1d056a03eec62d0216a4
emidio@morgia.info <emidio@morgia.info>
2021-02-28 19:14:11 +0100

    work in progress

--- src/main/java/info/morgia/javadddangular/HomeController.java
+++ src/main/java/info/morgia/javadddangular/HomeController.java
@@ -1,5 +1,9 @@
 package info.morgia.javadddangular;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/api/home")
 public class HomeController {
+        @PersistenceContext
+	protected EntityManager entityManager;
+        
         @GetMapping("/getValue")
         public String getValue() {
                 return "Test2";
+        }
+
+        @Transactional
+        @GetMapping("writeValues")
+        public String writeValues(){
+                Value v=new Value();
+                v.setIntValue(10);
+                v.setStringValue("ten");
+                entityManager.persist(v);
+                entityManager.flush();
+                entityManager.clear();
+                return "OK";
         }
 }
---
+++ src/main/java/info/morgia/javadddangular/Value.java
@@ -0,0 +1,40 @@
+package info.morgia.javadddangular;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "Values")
+public class Value {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int value){
+        id=value;
+    }
+
+    private int intValue;
+    public int getIntValue(){
+        return intValue;
+    }
+    public void setIntValue(int value){
+        intValue=value;
+    }
+
+    private String stringValue;
+    public String getStringValue(){
+        return stringValue;
+    }
+    public void setStringValue(String value){
+        stringValue=value;
+    }
+
+}
