94bb3bd9b78dc3a9c7bbc72c56ab4bb0f492d0c6
emidio@morgia.info <emidio@morgia.info>
2021-03-30 22:47:50 +0200

    work in progress with unit tests

--- clientapp/src/app/register/register.component.spec.ts
+++ clientapp/src/app/register/register.component.spec.ts
@@ -5,30 +5,82 @@ import { DebugElement } from '@angular/core';
 
 import { RegisterComponent } from './register.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let button :HTMLButtonElement;
 
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ],
-      imports : [RouterTestingModule.withRoutes([])]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegisterComponent ],
+      imports : [FormsModule, RouterTestingModule.withRoutes([])]
+    })
+    .compileComponents();
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    button=fixture.nativeElement.querySelector('#signupButton');
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call signupButtonClicked when button is clicked', fakeAsync(()=>{
+    spyOn(component,'signupButtonClicked').and.callThrough();
+
+    fixture.detectChanges();
+    tick();
+    button.click();
+    fixture.detectChanges();
+
+    expect(component.signupButtonClicked).toHaveBeenCalled();
+
+  }));
+
+  it('when inputEmail change value binds to email field', fakeAsync(()=>{
+
+    const hostElement = fixture.nativeElement;
+    const inputEmail: HTMLInputElement = hostElement.querySelector('#inputEmail');
+    fixture.detectChanges();
+    inputEmail.value = 'bbb';
+    inputEmail.dispatchEvent(new Event('input'));
+
+    expect(component.email).toBe('bbb');
+
+  }));
+
+  it('when inputEmail empty and button clicked class is-invalid not present', fakeAsync(()=>{
+
+    const hostElement = fixture.nativeElement;
+    const inputEmail: HTMLInputElement = hostElement.querySelector('#inputEmail');
+    fixture.detectChanges();
+    inputEmail.value = 'bbb';
+    inputEmail.dispatchEvent(new Event('input'));
+    tick();
+    button.click();
+    fixture.detectChanges();
+    expect(inputEmail.classList.contains('is-invalid')).toBeFalsy();
+
+  }));
+
+  it('when inputEmail not empty and button clicked class is-invalid is present', fakeAsync(()=>{
+
+    const hostElement = fixture.nativeElement;
+    const inputEmail: HTMLInputElement = hostElement.querySelector('#inputEmail');
+    fixture.detectChanges();
+    inputEmail.value = '';
+    inputEmail.dispatchEvent(new Event('input'));
+    tick();
+    button.click();
+    fixture.detectChanges();
+    expect(inputEmail.classList.contains('is-invalid')).toBeTruthy();
+
+  }));
 
 
   describe('signupButtonClicked', ()=>{
@@ -74,7 +126,5 @@ describe('RegisterComponent', () => {
   });
 
 });
-function queryDebugElement(arg0: string): HTMLInputElement {
-  throw new Error('Function not implemented.');
-}
+
 
